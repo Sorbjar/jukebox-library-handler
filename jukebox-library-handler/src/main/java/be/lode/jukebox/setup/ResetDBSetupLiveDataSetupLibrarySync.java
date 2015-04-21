a34ae06a2679e10b201a1ca715456ee44e9e4bc3
Lode Deckers <lode.deckers@gmail.com>
2015-04-21 16:54:48 +0200

    Extra handlers on main play page

---
+++ jukebox-library-handler/src/main/java/be/lode/jukebox/setup/ResetDBSetupLiveDataSetupLibrarySync.java
@@ -0,0 +1,28 @@
+package be.lode.jukebox.setup;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import be.lode.general.repository.Repository;
+import be.lode.jukebox.business.model.Song;
+import be.lode.jukebox.business.repo.SongRepository;
+import be.lode.jukebox.library.Library;
+import be.lode.setup.ResetDBSetupLiveData;
+
+public class ResetDBSetupLiveDataSetupLibrarySync {
+
+	public static void main(String[] args) {
+		ResetDBSetupLiveData.run();
+		run();
+	}
+
+	public static void run() {
+		Library lib = new Library();
+		lib.resyncLibrary();
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("jukebox-business");
+		Repository<Song> sRepo = new SongRepository(emf);
+		for (Song s : lib.getSongs()) {
+			sRepo.save(s);
+		}
+	}
+}
